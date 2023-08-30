b9bc77785f31bf237b1e6769dcb10885e0eaa1be
strannicamira <strannicamira@yandex.ru>
2023-08-30 19:46:15 +0300

    ewm: stat: Add swagger on https://editor-next.swagger.io Add @CrossOrigin(origins = "https://editor-next.swagger.io")

--- stat/stat-service/src/main/java/ru/practicum/StatController.java
+++ stat/stat-service/src/main/java/ru/practicum/StatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "https://editor-next.swagger.io")
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
