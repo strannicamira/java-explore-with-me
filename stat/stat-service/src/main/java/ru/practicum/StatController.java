377b5b4485456be437b575be245ceaf32cb1bdef
strannicamira <strannicamira@yandex.ru>
2023-08-30 19:31:07 +0300

    ewm: stat: Add swagger on http://localhost:9090/swagger-ui/index.html(clean up)

--- stat/stat-service/src/main/java/ru/practicum/StatController.java
+++ stat/stat-service/src/main/java/ru/practicum/StatController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-//@CrossOrigin(origins = "http://localhost:9090")
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
--- stat/stat-service/src/main/java/ru/practicum/StatServiceMain.java
+++ stat/stat-service/src/main/java/ru/practicum/StatServiceMain.java
@@ -1,29 +1,12 @@
 package ru.practicum;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-//@OpenAPIDefinition
 public class StatServiceMain {
     public static void main(String[] args) {
         System.out.println("Hello from stat-service!");
         SpringApplication.run(StatServiceMain.class, args);
     }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/hit").allowedOrigins("http://localhost:9090");
-//            }
-//        };
-//    }
 }
