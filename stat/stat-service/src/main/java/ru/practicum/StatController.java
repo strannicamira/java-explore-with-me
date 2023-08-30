c6dd37925db9b84ce17f66913cb068fd012067b8
strannicamira <strannicamira@yandex.ru>
2023-08-30 19:26:15 +0300

    ewm: stat: Add swagger (Black)

--- stat/stat-service/src/main/java/ru/practicum/StatController.java
+++ stat/stat-service/src/main/java/ru/practicum/StatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:9090")
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
