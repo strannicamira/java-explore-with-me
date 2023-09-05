42bbf6363b04969bcfad45f31795da80ce6bb336
strannicamira <strannicamira@yandex.ru>
2023-09-05 14:45:07 +0300

    ewm: ewm-service: event: Add findEventFullDtos (black) TODO: To test
    Postman:
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Event--> String length restrictions --> PATCH
    Изменение заголовка события с title.length < 3 (user endpoint)
    -
    Изменение события с description.length == 7000 && annotation.length == 2000 && title.length == 120 (user endpoint) = OK
    
    Skipped:
    Swagger:
    Postman:
    
    Sprint 17  main-svc Test Explore With Me - Main service --> Event -->
    Получение полной информации о событии добавленном текущим пользователем = OK
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Event--> Default values check --> Получение событий, добавленных текущим пользователем. Проверка на значения по-умолчанию size и from  = OK
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Event --> Получение событий, добавленных текущим пользователем без нескольких Query params = OK
    Sprint 17  main-svc Test Explore With Me - Main service --> Event --> Получение событий, добавленных текущим пользователем = OK
    
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Event--> Default values check --> Добавление нового события без paid, participantLimit, requestModeration
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Event--> Required params in body = OK
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Event--> String length restrictions = OK up to PATCH (TODO)
    Sprint 17  main-svc Test Explore With Me - Main service --> Event --> Добавление нового события = OK
    
    Sprint 17  main-svc Test Explore With Me - Main service --> 409 Conflict --> Category - OK up to DELETE (TODO)
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Category - OK
    Sprint 17  main-svc Test Explore With Me - Main service --> Category - OK
    
    Sprint 17  main-svc Test Explore With Me - Main service --> Validation --> Users - OK (Поиск событий без нескольких Query params - NOT OK) (TODO)
    Sprint 17  main-svc Test Explore With Me - Main service --> Users - OK

--- ewm/src/main/java/ru/practicum/user/User.java
+++ ewm/src/main/java/ru/practicum/user/User.java
@@ -14,9 +14,12 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 @Entity
 @Table(name = "users", schema = "public")
+@SequenceGenerator(name="user_id_seq", initialValue=0, allocationSize=1)
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue
+//    (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_id_seq")
     private Integer id;
     @NotBlank
     @Length(max = 250, min = 2)
